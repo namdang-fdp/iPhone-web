18dd4e47768bd842d3dfa522a5d68e998fa27953
namdang-fdp <namdangcoder@gmail.com>
2025-05-02 09:37:23 +0700

    update: gsap animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,40 @@ import { useEffect, useState } from 'react';
 
 const Hero = () => {
 
+	// const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
+  // const handleVideoSrcSet = () => {
+  //   if(window.innerWidth < 760) {
+  //     setVideoSrc(smallHeroVideo)
+  //   } else {
+  //     setVideoSrc(heroVideo)
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleVideoSrcSet);
+
+  //   return () => {
+  //     window.removeEventListener('reisze', handleVideoSrcSet)
+  //   }
+  // }, [])
+
+	useGSAP(() => {
+		gsap.to("#hero", {
+			opacity: 1,
+			delay: 1.5,
+			ease: 'power2.out',
+		})
+
+		gsap.to("#cta", {
+			opacity: 1,
+			y: 0,
+			delay: 1.5,
+			ease: 'expo.inOut',
+		})
+	}, [])
+
+
   return (
 		<section className="w-full nav-height bg-black relative">
 			<div className="h-5/6 w-full flex-center flex-col">
