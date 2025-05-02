d4527195673b1f74504fbc4ccc2d6fe162e38cb7
namdang-fdp <namdangcoder@gmail.com>
2025-05-02 09:27:16 +0700

    update: video and text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+import { useEffect, useState } from 'react';
 
 const Hero = () => {
-	return (
-    <div>Hero</div>
+
+  return (
+		<section className="w-full nav-height bg-black relative">
+			<div className="h-5/6 w-full flex-center flex-col">
+				<p id="hero" className="hero-title">IPhone 15 Pro</p>
+				<div className="md:w-10/12 w-9/12">
+					<video className="pointer-events-none" autoPlay muted playsInline={true} key={heroVideo}>
+						<source src={heroVideo} type="video/mp4" />
+					</video>
+				</div>
+			</div>
+
+			<div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+				<a href="#highlights" className="btn">Buy</a>
+				<p className="font-normal text-xl">From $199/month or $999</p>
+			</div>
+		</section>
   )
 }
 
 export default Hero
+
+
+
