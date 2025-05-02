a84d58d2cd69abdddc1a16c84f13c5d8325db0eb
namdang-fdp <namdangcoder@gmail.com>
2025-05-02 09:48:48 +0700

    update: responsive video set

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,24 +4,22 @@ import { heroVideo, smallHeroVideo } from '../utils';
 import { useEffect, useState } from 'react';
 
 const Hero = () => {
+	const [videoSrc, setVideoSrc] = useState(window.innerWidth > 768 ? heroVideo : smallHeroVideo);
+	
+	const handleVideoSrc = () => {
+		if(window.innerWidth <= 768) {
+			setVideoSrc(smallHeroVideo);
+		} else {
+			setVideoSrc(heroVideo);
+		}
+	}
 
-	// const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
-
-  // const handleVideoSrcSet = () => {
-  //   if(window.innerWidth < 760) {
-  //     setVideoSrc(smallHeroVideo)
-  //   } else {
-  //     setVideoSrc(heroVideo)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   window.addEventListener('resize', handleVideoSrcSet);
-
-  //   return () => {
-  //     window.removeEventListener('reisze', handleVideoSrcSet)
-  //   }
-  // }, [])
+	useEffect(() => {
+		window.addEventListener('resize', handleVideoSrc);
+		return () => {
+			window.removeEventListener('resize', handleVideoSrc);
+		}
+	}, [])
 
 	useGSAP(() => {
 		gsap.to("#hero", {
@@ -44,8 +42,8 @@ const Hero = () => {
 			<div className="h-5/6 w-full flex-center flex-col">
 				<p id="hero" className="hero-title">IPhone 15 Pro</p>
 				<div className="md:w-10/12 w-9/12">
-					<video className="pointer-events-none" autoPlay muted playsInline={true} key={heroVideo}>
-						<source src={heroVideo} type="video/mp4" />
+					<video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+						<source src={videoSrc} type="video/mp4" />
 					</video>
 				</div>
 			</div>
