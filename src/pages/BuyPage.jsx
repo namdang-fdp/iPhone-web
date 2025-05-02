7f686cb1e46ed022cd6c4e2cf2d69ca3433da994
namdang-fdp <namdangcoder@gmail.com>
2025-05-02 15:13:40 +0700

    update: step button progress

--- src/pages/BuyPage.jsx
+++ src/pages/BuyPage.jsx
@@ -8,6 +8,7 @@ import Navbar from '../components/Navbar';
 const BuyPage = () => {
 
 	const [isLoading, setIsLoading] = useState(true);
+	const [currentStep, setCurrentStep] = useState(1);
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
@@ -26,7 +27,18 @@ const BuyPage = () => {
 					<div className="w-12 h-12 rounded-full border-t-2 border-b-2 border-blue-500 animate-spin"></div>
 				</div>
 				) : (
-					<div>Hello</div>
+					<main className="container mx-auto px-4 py-8">
+						<div className="max-w-7xl max-auto">
+							<div className="flex items-center justify-between mb-8">
+								<h1 className="text-3xl md:text-4xl font-medium">iPhone 15 Pro</h1>
+								<div className="hidden md:flex items-center space-x-4">
+									<div className={`h-3 w-3 rounded-full ${currentStep >= 1 ? "bg-blue" : "bg-gray-600"}`}></div>
+									<div className={`h-3 w-3 rounded-full ${currentStep >= 2 ? "bg-blue" : "bg-gray-600"}`}></div>
+									<div className={`h-3 w-3 rounded-full ${currentStep >= 3 ? "bg-blue" : "bg-gray-600"}`}></div>
+								</div>
+							</div>
+						</div>
+					</main>
 				)
 			}
 		</div>
